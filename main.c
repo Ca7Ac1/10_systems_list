bbd9ca829a095679fce5f337e45648fe2bb5b596
Ca7Ac1 <Ca7Ac1@gmail.com>
2021-10-16 15:42:34 -0400

    Finished work

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define abs(a) a > 0 ? a : -1 * a
+
+struct Location
+{
+    char name[30];
+    float latitude;
+    float longitude;
+    struct Location *next;
+};
+
+struct Location *makeLocation(char *name, float latitude, float longitude)
+{
+    struct Location *l = malloc(sizeof(struct Location));
+
+    strcopy(l->name, name);
+    l->latitude = latitude;
+    l->longitude = longitude;
+    l->next = 0;
+
+    return l;
+}
+
+void printLocation(const struct Location *l)
+{
+    char lat = l->latitude >= 0 ? 'N' : 'S';
+    char lon = l->longitude >= 0 ? 'W' : 'E';
+
+    printf("%s is located at %.2f %c, %.2f %c", l->name, abs(l->latitude), lat, abs(l->longitude), lon);
+}
+
+void print_list(const struct Location *l)
+{
+    while (l)
+    {
+        printLocation(l);
+        printf("\n");
+
+        l = l->next;
+    }
+}
+
+struct Location *insert_front(struct Location *front, char *name, float latitude, float longitude)
+{
+    struct Location *l = makeLocation(name, latitude, longitude);
+    l->next = front;
+
+    return l;
+}
+
+struct Location *free_list(struct Location *l)
+{
+    struct Location *front = l;
+
+    while (l)
+    {
+        struct Location *prev = l;
+        l = l->next;
+
+        free(prev);
+    }
+
+    return front;
+}
+
+int main()
+{
+    srand(time(NULL));
+    struct Location *list = makeLocation("America", 37.0902, 95.7129);
+    list = insert_front(list, "Russia", 61.5240, -103.3188);
+    list = insert_front(list, "China", 35.8617, -104.1954);
+    list = insert_front(list, "Madeupland", (((float)rand() / (float)(RAND_MAX)) * 360) - 180, (((float)rand() / (float)(RAND_MAX)) * 360) - 180);
+
+    print_list(list);
+    free_list(list);
+}
